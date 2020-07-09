1846c786e037cf98c6cfa7ca567cf1305493e23e
ranaswami <54627057+ranaswami@users.noreply.github.com>
2020-07-10 01:41:22 +0530

    Day1_Q3_Checkith_1.java
    Using bit manipulation

---
+++ Day1_Q3_Checkith_1.java
@@ -0,0 +1,19 @@
+package spartank10x;
+
+import java.util.Scanner;
+
+public class Checkith1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num=sc.nextInt();
+		int pos=sc.nextInt();
+		System.out.println(checkith(num,pos));
+	}
+	public static boolean checkith(int x,int i) {
+		boolean isThere=false;
+		if((x|(1<<i))!=0)
+			isThere=true;
+		return isThere;
+	}
+}
