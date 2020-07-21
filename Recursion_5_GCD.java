5a978ebb4f6d99b93e888bba21829886e83de211
ranaswami <54627057+ranaswami@users.noreply.github.com>
2020-07-21 16:57:38 +0530

    Recursion_5_GCD.java

---
+++ Recursion_5_GCD.java
@@ -0,0 +1,18 @@
+package spartank10x;
+
+import java.util.Scanner;
+
+public class GCD {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println(gcd(a, b));
+	}
+	public static int gcd(int a,int b) {
+		if(b==0)
+			return a;
+		return gcd(b,a%b);
+	}
+}
